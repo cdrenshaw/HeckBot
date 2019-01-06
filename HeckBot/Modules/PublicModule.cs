6659e9dbc9dceb4e170f941c6141569f405d5b41
Chris Renshaw <chris@nerdarray.net>
2019-01-05 19:44:27 -0800

    Added help command.

--- HeckBot/Modules/PublicModule.cs
+++ HeckBot/Modules/PublicModule.cs
@@ -14,5 +14,15 @@ namespace HeckBot.Modules
             var msg = $@"Hi {Context.User}! This guild is being served by HeckBot shard number {Context.Client.GetShardFor(Context.Guild).ShardId}";
             await ReplyAsync(msg);
         }
+
+        [Command("help")]
+        public async Task HelpAsync()
+        {
+            var msg = $@"Below is a list of things I can do:
+                @HeckBot quests - Returns the current and upcoming quests
+                @HeckBot shield <time> - Starts a shield timer for <time> hours
+                @HeckBot shield stop - Stops all shield timers";
+            await ReplyAsync(msg);
+        }
     }
 }
